9193b7ca97ccf39032112665ca3517c5014cfbf7
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2024-05-26 10:10:19 +0600

    adding main method

--- src/Question_1/Payment.java
+++ src/Question_1/Payment.java
@@ -14,4 +14,18 @@ public class Payment {
         return paymentState.makePayment(amount);
     }
 
+    public static void main(String[] args) {
+        Payment paymentSystem = new Payment();
+        paymentSystem.setPaymentState(new BankPayment());
+        System.out.println("Payment: " + paymentSystem.makePayment(200));
+
+        paymentSystem.setPaymentState(new BkashPayment());
+        System.out.println("Payment : " + paymentSystem.makePayment(520));
+        paymentSystem.setPaymentState(new CreditPayment());
+        System.out.println("Payment: " + paymentSystem.makePayment(510));
+
+        paymentSystem.setPaymentState(new Nagad());
+        System.out.println("Payment: " + paymentSystem.makePayment((500)));
+    }
+
 }
