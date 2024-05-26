a797e41535e22d70c9d8118e19b6f305bb4acdf5
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2024-05-26 10:05:43 +0600

    adding payment method

--- src/Question_1/Payment.java
+++ src/Question_1/Payment.java
@@ -1,5 +1,17 @@
 package Question_1;
 
 public class Payment {
+    private PaymentState paymentState;
+
+    public void setPaymentState(PaymentState paymentState) {
+        this.paymentState = paymentState;
+    }
+    public boolean makePayment(double amount) {
+        if (paymentState == null) {
+            System.out.println("Payment nothings");
+            return false;
+        }
+        return paymentState.makePayment(amount);
+    }
 
 }
